76f3b96afd206897c403aed65eb8d976d1e94699
LucianaAG <lucygauto35@gmail.com>
2023-06-12 19:46:03 -0300

    fix: correcciones generales

--- work_trend_project/worktrend/asgi.py
+++ work_trend_project/worktrend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for my_project project.
+ASGI config for worktrend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'worktrend.settings')
 
 application = get_asgi_application()
